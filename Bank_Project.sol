f605db848ce92d346d27dd0fab7a03f9c61c007c
andresnv12 <avargas1424@gmail.com>
2024-10-29 18:48:02 -0600

    Se añadió código para las funciones addAuthorizedUser, isAuthorizedUser y getCustomerBalancer.

--- Bank_Project.sol
+++ Bank_Project.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.19;
 
@@ -24,6 +24,7 @@ contract Bank {
 
     constructor () {
         banker=msg.sender;
+        bankLedger[banker].authorizedUsers[msg.sender]=true;
         totalBalance=0;
     }
 
@@ -38,9 +39,9 @@ contract Bank {
     
     }
 
-    function getCustomerBalance () public view returns (uint) {
-
-    
+    function getCustomerBalance (address _customerAddress) public view returns (uint) {
+        
+        return bankLedger[_customerAddress].customerBalance;
 
     }
 
@@ -50,12 +51,15 @@ contract Bank {
     }
 
     function addAuthorizedUser  (address _customerAddress, address _addressToAdd) public  {
-    
-
+        require (banker==msg.sender, "Just the bankers can add users");
+        
+        //_addressToAdd para añadir nuevas cuentas y _customerAddress para cuentas dentro de authorizedUsers
+        bankLedger[_addressToAdd].authorizedUsers[_customerAddress] = true; 
+        
     }
 
     function isAuthorizedUser (address _address) public view returns (bool) {
-    
+        return bankLedger[_address].authorizedUsers[msg.sender];
     }
 
     
